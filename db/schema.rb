79bdbb455c2bd1122db02007dc824d9fd49f92a0
Yolanda M. Garcia <yolanda.garcia16@tectijuana.edu.mx>
2017-07-12 13:49:12 -0700

    Se a;adio una nueva vista, my portfolio y controllador, entro otras especificaciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630182840) do
+ActiveRecord::Schema.define(version: 20170704203143) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string   "ticket"
+    t.string   "name"
+    t.decimal  "last_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
